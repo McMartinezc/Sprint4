691919df0c04fca5df1eba09a4ef168a88510df2
McMartinezc <105592833+McMartinezc@users.noreply.github.com>
2022-09-13 10:49:52 +0200

    Update HelloWorldController.java

--- S4.01/S04T01N01MartinezMCarmen/src/main/java/controllers/HelloWorldController.java
+++ S4.01/S04T01N01MartinezMCarmen/src/main/java/controllers/HelloWorldController.java
@@ -1,23 +1,22 @@
 package controllers;
 
-import java.util.Optional;
+        import java.util.Optional;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+        import org.springframework.web.bind.annotation.*;
 
-
+@RestController
 public class HelloWorldController {
-	
-	@GetMapping("/helloWorld")
-	public String saluda(@RequestParam(value = "name", defaultValue = "UNKNOWN") String name) {
-	return String.format("Hello %s!, estàs executant un projecte Maven", name);
-	}
-	
-	@GetMapping("/helloWorld2")
-	public String saluda2(@PathVariable() Optional <String> name) {
-		String nameOpcional = name.orElse("UNKNOWN");
-	return String.format("Hello %s!, estàs executant un projecte Maven", nameOpcional);
-	}
+
+    @GetMapping("/helloWorld")
+    public String saluda(@RequestParam(value = "name", defaultValue = "UNKNOWN")String name) {
+        return String.format("Hello %s!, estàs executant un projecte Maven", name);
+    }
+
+    @GetMapping({"/helloWorld2","/helloWorld2/{name}"})
+    public String saluda2(@PathVariable()Optional<String>name) {
+        String nameOpcional = name.orElse("UNKNOWN");
+        return String.format("Hello %s!, estàs executant un projecte Maven", nameOpcional);
+    }
 
 }
+
