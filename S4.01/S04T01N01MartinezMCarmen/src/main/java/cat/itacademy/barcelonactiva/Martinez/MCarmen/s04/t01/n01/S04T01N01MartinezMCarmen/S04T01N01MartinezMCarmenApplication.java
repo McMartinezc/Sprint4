391ccef95f079cc3cf78a2976463db5e8dbcc69a
McMartinezc <105592833+McMartinezc@users.noreply.github.com>
2022-09-13 12:26:00 +0200

    Update S04T01N01MartinezMCarmenApplication.java

--- S4.01/S04T01N01MartinezMCarmen/src/main/java/cat/itacademy/barcelonactiva/Martinez/MCarmen/s04/t01/n01/S04T01N01MartinezMCarmen/S04T01N01MartinezMCarmenApplication.java
+++ S4.01/S04T01N01MartinezMCarmen/src/main/java/cat/itacademy/barcelonactiva/Martinez/MCarmen/s04/t01/n01/S04T01N01MartinezMCarmen/S04T01N01MartinezMCarmenApplication.java
@@ -3,14 +3,11 @@ package cat.itacademy.barcelonactiva.Martinez.MCarmen.s04.t01.n01.S04T01N01Marti
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class S04T01N01MartinezMCarmenApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(S04T01N01MartinezMCarmenApplication.class, args);
 	}
-	
-	
-	
+
 }
